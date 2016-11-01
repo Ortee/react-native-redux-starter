776dff372512dde97a0dd5aeef2a874c7b94a527
Mateusz <kontakt@mateuszoracz.pl>
2016-11-01 23:16:49 +0100

    add eslint

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import {Scene, Router} from 'react-native-router-flux';
+import App from './app';
+
+class AppContainer extends Component {
+  render() {
+    return (
+      <App />
+    );
+  }
+}
+
+export default AppContainer;
